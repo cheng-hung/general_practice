fb19822fe2bbfa225709ee6eab0786cd7537150c
Cheng-Hung Lin <clin1@bnl.gov>
2026-02-17 17:12:02 -0500

    search scan in tiled

--- h5py.py
+++ h5py.py
@@ -14,3 +14,23 @@ f = 'xxxxx.h5'
 f_h5 = h5py.File(f, 'r')
 
 get_dataset_keys(f_h5)
+
+
+
+
+def print_time_fn(fn_list: list, time_limit:list = [None, None]):
+
+    for fn in fn_list:
+        try:
+            f5 = h5py.File(fn, 'r')
+            key = 'entry/instrument/detector/count_time'
+            exp_t = np.asarray(f5[key])
+
+            if time_limit[0]==time_limit[1]==None:
+                print(f'{exp_t = }; {fn = }')
+
+            elif (exp_t>=time_limit[0]) and (exp_t<=time_limit[1]):
+                print(f'{exp_t = }; {fn = }')
+
+        except (OSError, KeyError):
+            pass
